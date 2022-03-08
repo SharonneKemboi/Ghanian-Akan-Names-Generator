4923c77bbd81d8773ead7b5511208cf59e719be7
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-03-08 21:29:33 +0300

    Remove unnecessary comments

--- script/main.js
+++ script/main.js
@@ -14,23 +14,6 @@ var male = document.getElementById("secondgender").checked;
 
 
 
-/*Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-
- where;
-
- CC - is the century digits. For example 1989 has CC = 19
-
- YY - is the Year digits (1989 has YY = 89)
-
- MM -  is the Month
-
- DD - is the Day of the month 
-
- mod - is the modulus function ( % )*/
-
-
- //use the syntax array.slice(start, end)-method that accepts two optional parameters
-
 var YY = year.slice(0, 2);
 var YY = year.slice(2, 4);
 var CC = (YY - 1) / 100 + 1;
